a56a8c78f751d60e182d2261fbaeacc4d266cf67
洪伟鹏 <hongweipeng@bonree.com>
2021-10-22 18:23:06 +0800

    setup.s

--- resource/oslab/linux-0.11/boot/setup.s
+++ resource/oslab/linux-0.11/boot/setup.s
@@ -28,7 +28,7 @@ begbss:
 .text
 
 entry start
-start:              ! setup.s 的代码已经搬到内存 0x90000 处，段寄存器值 0x9000
+start:              ! setup.s 的代码已经搬到内存 0x90200 处，段寄存器值 0x9020
 
 ! ok, the read went well so we get current cursor position and save it for
 ! posterity.
@@ -142,14 +142,16 @@ end_move:
 	mov	ds,ax
 	lidt	idt_48		! load idt with 0,0
 	lgdt	gdt_48		! load gdt with whatever appropriate
+	                    ! 通过 lgdt 指令从内存中读取 48 位的内存数据，存入 GDTR 寄存器
+	                    ! 48 位数据表示的是全局描述符表的位置和大小，低 32 位表示起始位置，高16位表示表的最后一个字节的偏移（表的大小-1）
 
 ! that was painless, now we enable A20
-
+! 8024 是键盘控制器，其输出端口 P2 用来控制 A20 地址线
 	call	empty_8042
-	mov	al,#0xD1		! command write
+	mov	al,#0xD1		! command write     ! D1 表示写数据到 8024 的 P2 端口
 	out	#0x64,al
 	call	empty_8042
-	mov	al,#0xDF		! A20 on
+	mov	al,#0xDF		! A20 on            ! 选通 A20 地址线
 	out	#0x60,al
 	call	empty_8042
 
@@ -160,6 +162,8 @@ end_move:
 ! rectify it afterwards. Thus the bios puts interrupts at 0x08-0x0f,
 ! which is used for the internal hardware interrupts as well. We just
 ! have to reprogram the 8259's, and it isn't fun.
+
+! 初始化 8259 (中断控制)， 一段非常机械化的程序
 
 	mov	al,#0x11		! initialization sequence
 	out	#0x20,al		! send it to 8259A-1
@@ -198,9 +202,15 @@ end_move:
 ! we let the gnu-compiled 32-bit programs do that. We just jump to
 ! absolute address 0x00000, in 32-bit protected mode.
 	mov	ax,#0x0001	! protected mode (PE) bit
-	lmsw	ax		! This is it!
+	lmsw	ax		! This is it!       ! 这条指令类似于 mov cr0, ax
+	                                    ! cr0 是一个非常酷的寄存器，它的最后一位如果是0，则是16位模式（实模式）；如果是1，则是32位模式（保护模式）
 	jmpi	0,8		! jmp offset 0 of segment 8 (cs)
-	                ! jmpi 0 8 进入保护模式，之后寻址模式发生改变
+	                ! jmpi 0 8 进入保护模式，之后寻址模式发生改变。跳转到的其实不是 0x00080 地址，而是 0 地址
+	                ! 寻址方式，对于地址 cs:ip
+	                ! 实模式下：(cs << 4) + ip
+	                ! 保护模式下：(根据 cs 查 gdt 表) + ip
+	                !           保护模式下的 cs 又被称为选择子，保护模式下的地址翻译可由硬件完成，
+	                !           查表结果是一个 32 位的地址，ip 也是一个 32 位的地址，两个地址相加还是32位
 
 ! This routine checks that the keyboard command queue is empty
 ! No timeout is used - if this hangs there is something wrong with
@@ -212,8 +222,9 @@ empty_8042:
 	jnz	empty_8042	! yes - loop
 	ret
 
-gdt:
-	.word	0,0,0,0		! dummy
+! 保护模式下的地址翻译， 初始化 gdt 表
+gdt:                    ! 全称是 Global Description Table  全局描述符表, gdt 表作用可看：https://blog.csdn.net/yeruby/article/details/39718119
+	.word	0,0,0,0		! dummy     ! 第一个表项为空，不使用
 
 	.word	0x07FF		! 8Mb - limit=2047 (2048*4096=8Mb)
 	.word	0x0000		! base address=0
@@ -225,12 +236,34 @@ gdt:
 	.word	0x9200		! data read/write
 	.word	0x00C0		! granularity=4096, 386
 
-idt_48:
+                        ! 每个 word 是16位，所以每个表项是 64 位，每个表项占 8 个字节
+                        ! 初始化后的 gdt 表项如下
+                        ! 第 0 个表项：.word	0,0,0,0
+                        ! 第 1 个表项：.word 0x07FF, 0x0000, 0x9A00, 0x00C0
+                        ! 第 2 个表项：.word 0x07FF, 0x0000, 0x9200, 0x00C0
+
+                        ! ** 若 cs = 8, ip 0 时，查表出来的基地址是多少？ **
+                        ! 8 表示 8 个字节，由于每个表项占 8 个字节，因此找到的是第 1 个表项。
+                        ! 表项结构及含义
+                        !
+                        !64                       55                            47                  39                      32
+                        ! | 段基地址 (BASE) 31..24| G | D | 0 | AV L| 限长 19..16| P | DPL | 1 | type | 段基地址 (BASE) 23..16 |
+                        ! |              段基地址 (BASE) 15..0                   |            段限长 (LIMIT) 15..0            |
+                        !31                        23                           15                   7                      0
+                        !
+                        ! 第 1 表项为：0x00C0-9A00-0000-07FF
+                        !               |      |   |
+                        !               |      |   | 取表项中 16 ~ 31 位作为 段地址的 0 ~ 15
+                        !              \ /   \ /  \ /
+                        ! 得到的段地址  0x00 - 00 - 0000  即 0x00000000 。又由于 ip =0 ，所以会跳到 0 地址去执行。
+                        ! 你会发现，第二个表项也是指向 0 地址，这两个表项，一个只读（代码），一个读写（数据）
+
+idt_48:                 ! IDT 表，中断处理函数入口
 	.word	0			! idt limit=0
-	.word	0,0			! idt base=0L
+	.word	0,0			! idt base=0L       ! 保护模式中断函数表，保护模式下的中断也是需要通过 idt 表查询处理函数
 
 gdt_48:
-	.word	0x800		! gdt limit=2048, 256 GDT entries
+	.word	0x800		! gdt limit=2048, 256 GDT entries    ! 共有 256 个 GDT 表项
 	.word	512+gdt,0x9	! gdt base = 0X9xxxx
 	
 .text
